50205810aafd1d4ffbc05d7986b11e304c4c91de
yourfriend <prabalshakya11@gmail.com>
2026-09-21 18:49:20 +0545

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-
+import { Analytics } from "@vercel/analytics/next";
 import Footer from "./components/Footer";
 import Gallery from "./pages/Gallery";
 import Projects from "./pages/Projects";
@@ -17,6 +17,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
+      <Analytics />
     </div>
   );
 }
